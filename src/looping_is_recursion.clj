be26b986c6cff5446b10b2ff4c16dd53983f0a11
s0rbus <soorbus@gmail.com>
2014-11-02 12:36:14 +0000

    finished ex2

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,10 +1,18 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [acc base e]
+                 (if (zero? e)
+                   acc
+                   (recur (* acc base) base (dec e))))]
+    (helper 1 base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [coll]
+                (if (< (count coll) 2)
+                  coll
+                  (recur (rest coll))))]
+    (first (helper a-seq))))
 
 (defn seq= [seq1 seq2]
   ":(")
